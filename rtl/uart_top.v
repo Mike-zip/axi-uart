aadbdd583232982b07e4cb4b3ba8f9335e4aafd3
Michael Marquis <michaelmarquis43@gmail.com>
2026-07-25 20:17:41 -0700

    Formatting changes for readability

--- rtl/uart_top.v
+++ rtl/uart_top.v
@@ -53,73 +53,73 @@ module Uart_Top #(
 
   //AXI4-Lite Interface
   Uart_Axi_Lite #(
-    .Address_Width(Address_Width),
-    .Data_Width(Data_Width)
+    .Address_Width   (Address_Width),
+    .Data_Width      (Data_Width)
   ) Axi_Lite (
-    .Clk(Clk), 
-    .Rst_N(Rst_N),
-    .Write_Address(Write_Address), 
-    .Write_Address_Valid(Write_Address_Valid), 
-    .Write_Address_Ready(Write_Address_Ready),
-    .Write_Data(Write_Data), 
-    .Write_Data_Valid(Write_Data_Valid), 
-    .Write_Data_Ready(Write_Data_Ready),
-    .Bresp(Bresp), 
-    .Bvalid(Bvalid), 
-    .Bready(Bready),
-    .Read_Address(Read_Address), 
-    .Read_Address_Valid(Read_Address_Valid), 
-    .Read_Address_Ready(Read_Address_Ready),
-    .Read_Data(Read_Data), 
-    .Rresp(Rresp), 
-    .Rvalid(Rvalid), 
-    .Rready(Rready),
-    .Tx_Data(Tx_Data), 
-    .Tx_Push_Enable(Tx_Push_Enable), 
-    .Tx_Full(Tx_Full), 
-    .Tx_Busy(Tx_Busy), 
-    .Tx_Occupancy(Tx_Occupancy),
-    .Rx_Byte(Rx_Byte), 
-    .Rx_Pop_Enable(Rx_Pop_Enable), 
-    .Rx_Ready(Rx_Ready),
-    .Frame_Error(Frame_Error), 
-    .Over_Run_Error(Over_Run_Error), 
-    .Rx_Occupancy(Rx_Occupancy)
+    .Clk                  (Clk), 
+    .Rst_N                (Rst_N),
+    .Write_Address        (Write_Address), 
+    .Write_Address_Valid  (Write_Address_Valid), 
+    .Write_Address_Ready  (Write_Address_Ready),
+    .Write_Data           (Write_Data), 
+    .Write_Data_Valid     (Write_Data_Valid), 
+    .Write_Data_Ready     (Write_Data_Ready),
+    .Bresp                (Bresp), 
+    .Bvalid               (Bvalid), 
+    .Bready               (Bready),
+    .Read_Address         (Read_Address), 
+    .Read_Address_Valid   (Read_Address_Valid), 
+    .Read_Address_Ready   (Read_Address_Ready),
+    .Read_Data            (Read_Data), 
+    .Rresp                (Rresp), 
+    .Rvalid               (Rvalid), 
+    .Rready               (Rready),
+    .Tx_Data              (Tx_Data), 
+    .Tx_Push_Enable       (Tx_Push_Enable), 
+    .Tx_Full              (Tx_Full), 
+    .Tx_Busy              (Tx_Busy), 
+    .Tx_Occupancy         (Tx_Occupancy),
+    .Rx_Byte              (Rx_Byte), 
+    .Rx_Pop_Enable        (Rx_Pop_Enable), 
+    .Rx_Ready             (Rx_Ready),
+    .Frame_Error          (Frame_Error), 
+    .Over_Run_Error       (Over_Run_Error), 
+    .Rx_Occupancy         (Rx_Occupancy)
   );
 
 
   //Tx
   Uart_Tx #(
-    .Clk_Frequency(Clk_Frequency),
-    .Baud_Rate(Baud_Rate),
-    .Fifo_Slots(Fifo_Slots)
+    .Clk_Frequency  (Clk_Frequency),
+    .Baud_Rate      (Baud_Rate),
+    .Fifo_Slots     (Fifo_Slots)
   ) Transmitter (
-    .Clk(Clk), 
-    .Rst_N(Rst_N),
-    .Data_In(Tx_Data),
-    .Push_Enable(Tx_Push_Enable),
-    .Tx_Full(Tx_Full),
-    .Occupancy(Tx_Occupancy),
-    .Tx_Busy(Tx_Busy),
-    .Tx(Uart_Txd)
+    .Clk          (Clk), 
+    .Rst_N        (Rst_N),
+    .Data_In      (Tx_Data),
+    .Push_Enable  (Tx_Push_Enable),
+    .Tx_Full      (Tx_Full),
+    .Occupancy    (Tx_Occupancy),
+    .Tx_Busy      (Tx_Busy),
+    .Tx           (Uart_Txd)
   );
 
   //Rx
   Uart_Rx #(
-    .Clk_Frequency(Clk_Frequency),
-    .Baud_Rate(Baud_Rate),
-    .Over_Sample(Over_Sample),
-    .Fifo_Slots(Fifo_Slots)
+    .Clk_Frequency  (Clk_Frequency),
+    .Baud_Rate      (Baud_Rate),
+    .Over_Sample    (Over_Sample),
+    .Fifo_Slots     (Fifo_Slots)
   ) Receiver (
-    .Clk(Clk), 
-    .Reset(Rst_N),            
-    .Rx_Data(Uart_Rxd),
-    .Pop_Enable(Rx_Pop_Enable),
-    .Rx_Ready(Rx_Ready),
-    .Occupancy(Rx_Occupancy),
-    .Frame_Error(Frame_Error),
-    .Over_Run_Error(Over_Run_Error),
-    .Data_Out(Rx_Byte)                  
+    .Clk             (Clk), 
+    .Reset           (Rst_N),            
+    .Rx_Data         (Uart_Rxd),
+    .Pop_Enable      (Rx_Pop_Enable),
+    .Rx_Ready        (Rx_Ready),
+    .Occupancy       (Rx_Occupancy),
+    .Frame_Error     (Frame_Error),
+    .Over_Run_Error  (Over_Run_Error),
+    .Data_Out        (Rx_Byte)                  
   );
 
 
